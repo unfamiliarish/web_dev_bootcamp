aee7cce7c80143c28e0147d83711bd7c0f0eb515
unfamiliarish <89762294+unfamiliarish@users.noreply.github.com>
2023-01-10 19:09:27 -0800

    added quit option

--- s19/guessing_game/app.js
+++ s19/guessing_game/app.js
@@ -4,20 +4,30 @@ while (!max_num) {
     max_num = parseInt(prompt("Enter the maximum number!"));
 }
 
+console.log(`max: ${max_num}`);
 answer = Math.floor(Math.random() * max_num) + 1
+console.log(`answer: ${answer}`);
 
 let guess, num_guesses = 0;
-while (guess !== answer) {
-    num_guesses++;
+while (parseInt(guess) !== answer) {
+    console.log(`guess: ${guess}`);
+    if (guess === "q") break;
 
+    num_guesses++;
+    guess = parseInt(guess)  // not necessary, helps with readability
     if (!guess || guess > max_num || guess < 1) {
-        guess = parseInt(prompt(`Guess a number 1-${max_num}`));
+        guess = prompt(`Guess a number 1-${max_num}`);
     } else if (guess > answer) {
-        guess = parseInt(prompt("Your number is too high!"));
+        guess = prompt("Your number is too high!");
     } else if (guess < answer) {
-        guess = parseInt(prompt("Your number is too low!"));
+        guess = prompt("Your number is too low!");
     }
 }
 
-alert(`You got it in ${num_guesses} guesses!`);
-
+if (guess === "q") {
+    console.log("exited");
+    alert("Successfully exited.");
+} else {
+    console.log(`${num_guesses} guesses!`);
+    alert(`You got it in ${num_guesses} guesses!`);
+}
