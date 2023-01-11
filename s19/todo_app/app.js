7a24e3fa675bc1764249dbc33efb2c9b4e2152c1
unfamiliarish <89762294+unfamiliarish@users.noreply.github.com>
2023-01-10 20:06:53 -0800

    working todo app

---
+++ s19/todo_app/app.js
@@ -0,0 +1,25 @@
+/* <li>"new" - Add a todo</li>
+<li>"list" - List all todos</li>
+<li>"delete" - Remove todo by index</li>
+<li>"quit" - Quit app</li> */
+
+let todos = [];
+let command;
+while (command !== "quit") {
+    command = prompt("Enter your command:");
+    if (command === "new") {
+        let new_todo = prompt("Enter a new todo:");
+        todos.push(new_todo);
+    } else if (command == "list") {
+        console.log("********")
+        for (const [i, todo] of todos.entries()) {
+            console.log(`${i}: ${todo}`);
+        }
+        console.log("********");
+    } else if (command == "delete") {
+        let index = prompt("Enter index of todo you want to delete:");
+        todos.splice(index, 1);
+    }
+}
+console.log("Successfully exited.");
+alert("Successfully exited.");
