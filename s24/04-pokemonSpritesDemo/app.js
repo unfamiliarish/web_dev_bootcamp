99c47fd760d3c8208869e6ae70b30e488f8a2171
unfamiliarish <89762294+unfamiliarish@users.noreply.github.com>
2023-02-03 11:17:58 -0800

    my version pokemon demo

---
+++ s24/04-pokemonSpritesDemo/app.js
@@ -0,0 +1,32 @@
+// pokemon imgs url, replace 1 to get different sprites
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png 
+const basePokemonUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+
+const container = document.querySelector("#pokemon");
+container.classList.add("d-flex", "flex-wrap");
+
+for (let i = 1; i <= 151; i++) {
+    const newFigure = document.createElement("figure");
+    newFigure.classList.add(
+        "col",
+        "figure-image",
+        "img-fluid",
+        "img-rounded",
+        "img-thumbnail",
+        "bg-secondary-subtle",
+        "text-center",
+        "m-1",
+    );
+
+    const newImg = document.createElement("img");
+    newImg.src = `${basePokemonUrl}${i}.png`;
+
+    const newCaption = document.createElement("figcaption");
+    newCaption.classList.add("figure-caption");
+    newCaption.innerText = `${i}`;
+
+
+    newFigure.append(newImg, newCaption);
+    container.appendChild(newFigure);
+}
+
