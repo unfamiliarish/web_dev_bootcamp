c441b89ac2d8477e7dffec1250daebb16413dfce
unfamiliarish <89762294+unfamiliarish@users.noreply.github.com>
2023-02-13 12:17:23 -0800

    preventDefault

---
+++ s25/05_form_events_and_prevent_default/app.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName");
+const catList = document.querySelector("#cats");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("submit");
+
+    const cat = document.createElement("li");
+    cat.innerText = input.value;
+
+    // clear the input field after submit
+    input.value = "";
+
+    catList.appendChild(cat);
+})
+
